c62237547b647e73ad12792b7a225a8f3b08e201
goo-goo-goo-joob <pro1895sto@gmail.com>
2020-09-05 00:27:53 +0300

    forms and tables

--- db_cinema_project/__main__.py
+++ db_cinema_project/__main__.py
@@ -1,21 +1,90 @@
 import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
 
-from db_cinema_project.ui import project  # Это наш конвертированный файл дизайна
+from db_cinema_project.db import utils
+from db_cinema_project.ui import auth_form, \
+    main_form, main  # Это наш конвертированный файл дизайна
 
 
-# pylint: disable=I1101
-class ExampleApp(QtWidgets.QMainWindow, project.Ui_MainWindow):
+class Table(QtWidgets.QWidget):
+    def __init__(self, res):
+        super().__init__()
+        self.title = 'Таблица сторудников'
+        self.left = 0
+        self.top = 0
+        self.width = 1000
+        self.height = 700
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.tableWidget = QtWidgets.QTableWidget()
+        self.tableWidget.setRowCount(len(res))
+        self.tableWidget.setColumnCount(len(res[0]))
+
+        for row_number, row_data in enumerate(res):
+            for column_number, data in enumerate(row_data):
+                self.tableWidget.setItem(row_number, column_number,
+                                         QtWidgets.QTableWidgetItem(str(data)))
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.tableWidget)
+        self.setLayout(self.layout)
+
+
+class MainApp(QtWidgets.QMainWindow, main.Ui_MainWindow):
+    def __init__(self, db):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton.clicked.connect(self.print_db)
+        self.db = db
+
+    def print_db(self):
+        res = self.db.get_all_stuff()
+        self.table = Table(res)
+        self.table.show()
+
+
+class AuthApp(QtWidgets.QMainWindow, auth_form.Ui_MainWindow):
     def __init__(self):
         # Это здесь нужно для доступа к переменным, методам
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.authButton.clicked.connect(self.auth)
+        self.dialog = QtWidgets.QErrorMessage()
+
+        self.msg = QtWidgets.QMessageBox()
+        self.msg.setIcon(QtWidgets.QMessageBox.Information)
+        self.msg.setText("Не удалось подключиться.")
+        self.msg.setWindowTitle("Ошибка подключения")
+        self.msg.setStandardButtons(
+            QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+        self.msg.buttonClicked.connect(self.ok)
+
+        self.db = None
+
+    def ok(self):
+        self.msg.close()
+
+    def auth(self):
+        login = self.login.text()
+        password = self.password.text()
+        try:
+            self.db = utils.DBCinema('localhost', login, password,
+                                     'labs')
+            self.close()
+            self.Open = MainApp(self.db)
+            self.Open.show()
+        except RuntimeError:
+            self.msg.show()
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
-    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window = AuthApp()  # Создаём объект класса ExampleApp
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
 
--- db_cinema_project/db/utils.py
+++ db_cinema_project/db/utils.py
@@ -1,6 +1,7 @@
 import os
 
 import pymysql
+from PyQt5 import QtSql
 
 
 class DBCinema:
@@ -10,12 +11,22 @@ class DBCinema:
                                     password=password,
                                     db=database,
                                     charset='utf8mb4',
-                                    cursorclass=pymysql.cursors.DictCursor)
+                                    cursorclass=pymysql.cursors.Cursor)
+        # self.db = QtSql.QSqlDatabase.addDatabase('QMYSQL')
+        # self.db.setHostName(host)
+        # self.db.setDatabaseName('labs')
+        # self.db.setUserName(user)
+        # self.db.setPassword(password)
+        # self.db.open()
 
     def close(self):
         self.conn.close()
 
     def get_all_stuff(self):
+        # self.query = QtSql.QSqlQuery(self.db)
+        # self.query.prepare("SELECT * FROM сотрудники")
+        # self.query.exec()
+        # return self.query.result()
         with self.conn.cursor() as c:
             sql = "SELECT * FROM сотрудники "
             c.execute(sql)
@@ -25,5 +36,5 @@ class DBCinema:
 if __name__ == '__main__':
     db = DBCinema(os.getenv('DB_HOST'), os.getenv('DB_USER'), os.getenv('DB_PASSWORD'),
                   os.getenv('DB_DATABASE'))
-    print(*db.get_all_stuff(), sep='\n==================\n')
+    # print(*db.get_all_stuff(), sep='\n==================\n')
     db.close()
